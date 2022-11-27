743b7f86f8a59b3d35cc76a85d27733fe12e370d
Thomas Labbe <thomas.labbe.1@ulaval.ca>
2022-11-26 20:27:39 -0500

    final commit of the night

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ from Eletromyogram_analysis import Electromyogram_analysis
 path = 'all_data/data_CapgMyo/matlab_format'
 f_types = 'mat'
 test = Electromyogram_analysis(path, f_types)
-test.format_mat_files(window_length=950, name_of_txt_file = 'first_data_set_', overwrite = False)
+test.format_mat_files(window_length=100, name_of_txt_file = 'first_data_set_', overwrite = False)
 test.normalize_set()
-test.plot_emg_signal_and_fft(test.emg_data['1'].get('data')[0][0])
-test.plot_hitogram_mvmnts('1')
-test.plot_jeu_2_electrodes(subject='1', ch0=6, ch1=17, classes='all', legend_with_name=False)
+#test.plot_emg_signal_and_fft(test.emg_data['1'].get('data')[0][0])
+#test.plot_hitogram_mvmnts('1')
+
+for i in range(1, 5):
+    test.plot_jeu_2_electrodes(subject=f'{i}', ch0=1, ch1=125, classes=[1, 2, 3], legend_with_name=False)
